c970169724ae0dad2b13cee9a9a8760caeed1840
Alexander Knapen <alexander.w.knapen@gmail.com>
2021-04-21 21:19:16 -0700

    Data forwarding working on R type instructions

--- lab2/tests/testU_Haz.s
+++ lab2/tests/testU_Haz.s
@@ -1,21 +1,21 @@
-#This file contains tests for R-type RISCV instructions WITH NO HAZARDS (by this I mean I added nops to prevent hazards)
+#This file contains tests for R-type RISCV instructions WITH HAZARDS
 #You can check to see if any of your tests fail using this link: https://piazza.com/class/kbprd1f9gqj5ro?cid=13
 
 .data
 	TEST: .word 0x1000770
-	
+
 .text
 
 	#Setup stuff
 	addi a0, zero, 50 #50 is an arbitrary number, most tests will end up with a result of 50 to keep things simple
 	addi a1, zero, 1
-	
-tests:	
+
+tests:
 	nop
 	nop
-	
+
 	#Load upper immediate test
-	auipc	t0,0x0 
+	auipc	t0,0x0
 	nop
 	nop
 	addi	t0,t0,88 # PC+88= 0x68 <TEST>
@@ -33,7 +33,7 @@ tests:
 	bne t1, t2, fail
 	nop
 	nop
-	
+
 	j tests
 	nop
 	nop
