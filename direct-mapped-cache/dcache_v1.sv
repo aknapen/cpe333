b66ea502d438ef5b83e563e1f28ec57f0549e025
Alexander Knapen <alexander.w.knapen@gmail.com>
2021-05-04 08:13:05 -0700

    added initial files for cache lab

---
+++ direct-mapped-cache/dcache_v1.sv
@@ -0,0 +1,166 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Keefe Johnson
+//           Joseph Callenes
+//           
+// 
+// Create Date: 02/06/2020 06:40:37 PM
+// Design Name: 
+// Module Name: dcache
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Revision 0.02 - 
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+package cache_def;
+
+parameter int TAG_MSB = 31;
+parameter int TAG_LSB = 12;
+
+typedef struct packed{
+    logic valid;
+    logic dirty;
+    logic [TAG_MSB:TAG_LSB] tag;
+}cache_tag_type;
+
+typedef struct {
+    logic [9:0] index;
+    logic we;
+}cache_req_type;
+
+//128-bit cache line
+typedef logic [127:0] cache_data_type;
+
+//CPU request (CPU ->cache controller)
+typedef struct{
+    logic [31:0] addr;
+    logic [31:0] data;
+    logic rw;
+    logic valid;
+}cpu_req_type;
+
+//Cache result (cache controller -> CPU)
+typedef struct {
+    logic [31:0]data;
+    logic ready;
+}cpu_result_type;
+
+//memory request (cache controller -> memory)
+typedef struct {
+    logic [31:0]addr;
+    logic [127:0]data;
+    logic rw;
+    logic valid;
+}mem_req_type;
+
+//memory controller response (memory -> cache controller)
+typedef struct {
+cache_data_type data;
+logic ready;
+}mem_data_type;
+
+endpackage
+
+import cache_def::*;
+import memory_bus_sizes::*; 
+
+module L1_cache_data ( 
+    input clk,
+    input cache_req_type data_req,
+    input cache_data_type data_write,
+    input [3:0] be,
+    input [1:0] block_offset,
+    input from_ram,
+    output cache_data_type data_read);
+    
+    cache_data_type data_mem[0:255];
+    
+    initial begin
+        for(int i=0; i<256; i++)
+            data_mem[i]='0;
+    end
+
+    always_ff @(posedge clk) begin
+        if(data_req.we) begin
+            if(from_ram) 
+                data_mem[data_req.index] <= data_write;
+            if(!from_ram) begin
+              for (int b = 0; b < WORD_SIZE; b++) begin
+                if (be[b]) begin
+                    data_mem[data_req.index][block_offset*WORD_WIDTH+b*8+:8] <= data_write[block_offset*WORD_WIDTH+b*8+:8];  //[b*8+:8];
+                end
+              end
+            end
+        end
+            
+        data_read <= data_mem[data_req.index];
+    end
+endmodule
+
+module L1_cache_tag (
+    input logic clk,
+    input cache_req_type tag_req,
+    input cache_tag_type tag_write,
+    output cache_tag_type tag_read);
+    
+	// tag storage 
+	// incluces valid and dirty bits
+	// async read, sync write
+	
+	
+endmodule
+
+
+module dcache(
+    input clk, RESET,
+    i_mhub_to_dcache.device mhub,
+    i_dcache_to_ram.controller ram
+    );
+
+    cpu_req_type cpu_req;     //CPU->cache
+    mem_data_type mem_data;   //memory->cache
+    
+    mem_req_type mem_req;    //cache->memory
+    cpu_result_type cpu_res;  //cache->CPU
+    
+    logic [1:0] block_offset;
+    logic [3:0] be;
+    logic from_ram;
+    logic wait_read, next_wait_read;   
+    
+    
+    typedef enum {idle, compare_tag, allocate, writeback} cache_state_type;
+   
+    cache_state_type state, next_state;
+
+    cache_tag_type tag_read;
+    cache_tag_type tag_write;
+    cache_req_type tag_req;
+    
+    cache_data_type data_read;
+    cache_data_type data_write;
+    cache_req_type data_req;
+    
+    cpu_result_type next_cpu_res;
+    
+	
+	//FSM for Cache Controller
+
+	
+	
+	
+
+    L1_cache_tag L1_tags(.*);
+    L1_cache_data L1_data(.*);
+
+endmodule
